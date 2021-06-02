0c48e035e8d11484e6979da6d87d33a1a9641f5c
Alena Sidorova <sidsidsid1111@gmail.com>
2021-06-02 10:59:19 +0300

    get_delivery_date fixed

--- pages/main_page.py
+++ pages/main_page.py
@@ -10,15 +10,16 @@ class MainPage(BasePage):
     def get_delivery_date(self, browser):
         page = MainPage(browser, self.URL)
         page.open_page()
-        delivery_date_value = browser.find_element(*MainPageLocators.DELIVERY_DATE).text
-        deliv_date = delivery_date_value[13:].split(" ")
+        delivery_date_value = browser.execute_script('return arguments[0].textContent;', browser.find_element(*MainPageLocators.DELIVERY_DATE))
+        deliv_date = delivery_date_value.strip().split(" ")
         months = {"января": 1, "февраля": 2, "мар": 3, "апр": 4,
                   "мая": 5, "июня": 6, "июл": 7, "авг": 8,
                   "сен": 9, "окт": 10, "ноя": 11, "дек": 12}
-        site_date = int(deliv_date[0])
+        site_date = int(deliv_date[-2])
+
         site_month = 0
         for k, v in months.items():
-            if k.startswith(deliv_date[1][:3]):
+            if k.startswith(deliv_date[-1][:3]):
                 site_month += v
 
         return datetime.datetime(datetime.datetime.now().year, int(site_month), site_date)
